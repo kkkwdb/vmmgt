198cb7bcdce497060dd0db6328bd88ce184d4e79
wangdb <wangdb@sugon.com>
2018-07-24 10:03:22 +0800

    add verbose option

--- list_vm.go
+++ list_vm.go
@@ -23,15 +23,24 @@ var listCmd = cli.Command{
 	Aliases: []string{"l"},
 	Usage:   "list virtual machines",
 	Action:  list_vm,
+	Flags: []cli.Flag{
+		cli.BoolFlag{
+			Name:  "verbose,v",
+			Usage: "Display more vm information",
+		},
+	},
 }
 
 func list_vm(c *cli.Context) {
+	verbose := c.Bool("verbose")
 	machines := c.GlobalString("name")
 	doms, err := virtConn.ListAllDomains(0)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	vcpus := make(map[string]uint)
+	memories := make(map[string]uint64)
 	states := make(map[string]int)
 	orderdNames := make([]string, 0)
 	for _, dom := range doms {
@@ -47,12 +56,29 @@ func list_vm(c *cli.Context) {
 			}
 			states[name] = int(state)
 			orderdNames = append(orderdNames, name)
+			if verbose {
+				di, err := dom.GetInfo()
+				if err != nil {
+					log.Fatal(err)
+				}
+				memories[name] = di.Memory
+				vcpus[name] = di.NrVirtCpu
+			}
 		}
 
 		dom.Free()
 	}
+	sort.Strings(orderdNames)
 	sort.Slice(orderdNames, func(i, j int) bool { return states[orderdNames[i]] < states[orderdNames[j]] })
+
+	if verbose {
+		fmt.Printf("%-8s\t%s\t%s\t%s\n", "name", "state", "cpu", "memory")
+		for _, name := range orderdNames {
+			fmt.Printf("%-8s\t%s\t%d\t%d\n", name, stateTable[states[name]], vcpus[name], memories[name])
+		}
+		return
+	}
 	for _, name := range orderdNames {
-		fmt.Printf("%-16s\t%s\n", name, stateTable[states[name]])
+		fmt.Printf("%-8s\t%s\n", name, stateTable[states[name]])
 	}
 }
